36e28b79cb48cc7c182664b40b373d27f0c6b9e7
MMichaelKK <michael.323449@nv.ccsd.net>
2023-02-09 13:36:36 -0800

    Changed launcher Version

--- Doll House/Assets/Scripts/PlayerMovment.cs
+++ Doll House/Assets/Scripts/PlayerMovment.cs
@@ -4,38 +4,45 @@ public class PlayerMovment : MonoBehaviour
 {
     public float speed = 0f;
     public float Rspeed = 0f;
-    public GameObject camera;
     float mosPosX;
     float newMosPosX;
     Vector3 mousePos;
+    public GameObject camera;
     Transform cameratransform;
     // Start is called before the first frame update
     void Start()
     {
+        cameratransform = camera.transform;
         mousePos = Input.mousePosition;
         mosPosX = mousePos.x;
+        Cursor.visible = false;
+        Cursor.lockState = CursorLockMode.Locked;
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKey(KeyCode.W)) {
+        if (Input.GetKey(KeyCode.W))
+        {
             transform.Translate(Vector3.forward * speed * Time.deltaTime);
         }
-        else if(Input.GetKey(KeyCode.S)){
+        else if (Input.GetKey(KeyCode.S))
+        {
             transform.Translate(Vector3.back * speed * Time.deltaTime);
         }
-        if (Input.GetKey(KeyCode.A)) {
-            transform.Rotate(Vector3.left * speed * Time.deltaTime);
+        if (Input.GetKey(KeyCode.A))
+        {
+            transform.Translate(new Vector3(-1, 0, 0) * speed * Time.deltaTime);
         }
         else if (Input.GetKey(KeyCode.D))
         {
-            transform.Rotate(Vector3.right * speed * Time.deltaTime);
+            transform.Translate(new Vector3(1, 0, 0) * speed * Time.deltaTime);
         }
-        //mousePos = Input.mousePosition;
-        //newMosPosX = mousePos.x;
-        //cameratransform.Rotate(Rspeed * Time.deltaTime * new Vector3((mosPosX - newMosPosX),0, 0));
-       // mosPosX = newMosPosX;
+        mousePos = Input.mousePosition;
+        newMosPosX = mousePos.x;
+        transform.Rotate(Rspeed * Time.deltaTime * new Vector3(0, (mosPosX - newMosPosX), 0));
+        mosPosX = newMosPosX;
+        Cursor.lockState = CursorLockMode.Locked;
 
     }
 }
